cfa48b6d2ec350706c0bd8b973b73aa377eb747c
serg <torlopovsg@e1.ru>
2022-08-09 17:21:54 +0500

    Вторая домашняя работа

---
+++ program.cs
@@ -0,0 +1,58 @@
+// задача 10
+
+//первый способ решения задачи    
+// Console.Write("Введите трехзначное число: ");
+
+// string number = Console.ReadLine();
+
+// Console.WriteLine(number[1]);
+
+
+
+// второй способ решения задачи
+// Console.Write("Введите число: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int firstPart = number / 10;
+
+// int middleNumber = firstPart % 10;
+
+// Console.WriteLine(middleNumber);
+
+
+// задача 13
+
+// Console.Write("Введите любое число: ");
+
+// string number = Console.ReadLine();
+
+// if(number.Length >= 3)
+// {
+//   Console.WriteLine($"Третья цифра - {number[2]}");
+// }
+// else
+// {
+//    Console.WriteLine("третьей цифры нет!");
+// }
+
+ 
+
+// задача 15
+
+// Console.Write("Введите цифру дня недели: ");
+
+// int digital = Convert.ToInt32(Console.ReadLine());
+
+// if(digital >= 1 && digital <= 5)
+// {
+//   Console.WriteLine("нет!");
+// }
+// else if(digital == 6 || digital == 7)
+// {
+//   Console.WriteLine("да! это выходной.");
+// }
+// else
+// {
+//   Console.WriteLine("неизвестный день!");
+// }
